5d29a53e70856b5683f800d4963d156b666e418a
myrma <skolopedrion@gmail.com>
2016-05-25 13:13:02 +0200

    Implemented comment line and comment block support

--- acid/lexer.py
+++ acid/lexer.py
@@ -10,6 +10,7 @@ __all__ = ['TokenType', 'Token', 'tokenize']
 
 import re
 from enum import Enum
+from itertools import dropwhile
 
 from acid.types import SourcePos
 from acid.exception import ParseError
@@ -26,6 +27,8 @@ class TokenType(Enum):
 
 	DEFINE = r'define'
 	LAMBDA = r'lambda'
+	LINE_COMMENT = r'//'
+	COMMENT_START, COMMENT_END = r'/\*', r'\*/'
 	LPAREN, RPAREN = r'\(', r'\)'
 	FLOAT_LITERAL = r'\d+\.\d+'
 	INT_LITERAL = r'\d+'
@@ -52,7 +55,7 @@ def tokenize(code):
 	"""
 	Chop the given string in Token instances.
 	"""
-	
+
 	cursor = SourcePos(line=1, column=1)
 
 	while code:
@@ -64,18 +67,40 @@ def tokenize(code):
 				# pop the matched string
 				code = code[match.end():]
 
+				# value is assigned to the entire match string
+				value = match.group(0)
+
+				# update cursor position (line and column index)
+				cursor.feed(value)
+
+				if token_type == TokenType.LINE_COMMENT:
+					# drop every character until newline
+					code = ''.join(dropwhile(lambda c: c != '\n', code))
+
+				elif token_type == TokenType.COMMENT_START:
+					# test if the code matches a comment ending token
+					m = TokenType.COMMENT_END.regex.match(code)
+
+					# while the comment block is not ended
+					while m is None:
+						# feed a character from the comment string
+						cursor.feed(code[0])
+
+						# pop a single character
+						code = code[1:]
+
+						# retest if the code matches a comment ending token
+						m = TokenType.COMMENT_END.regex.match(code)
+
+					# pop the matched string
+					code = code[m.end():]
+
 				# skipping whitespace
-				if token_type is not TokenType.WHITESPACE:
-					# value is assigned to the entire match string
-					value = match.group(0)
+				elif token_type is not TokenType.WHITESPACE:
+						# copy the cursor to avoid unwanted reference
+						pos = cursor.copy()
 
-					# update cursor position (line and column index)
-					cursor.feed(value)
-
-					# copy the cursor to avoid unwanted reference
-					pos = cursor.copy()
-
-					yield Token(token_type, value, pos)
+						yield Token(token_type, value, pos)
 
 				break
 		else:
